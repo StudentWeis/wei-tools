d01a200a9f97950c4c33b61ccb476f32f0b701b9
StudentWeis <studentweis@gmail.com>
2022-05-19 11:46:53 +0800

    video2image

--- auto/snipaste_auto.ps1
+++ auto/snipaste_auto.ps1
@@ -1,3 +1,3 @@
 $MyDataYearMouthDay = Get-Date -Format 'yyyy-M-d'
 $MyDataHourMinute = Get-Date -Format 'H-m'
-C:\Users\stude\scoop\apps\snipaste-beta\current\Snipaste.exe snip --full -o D:\TEMP\PicLog\$MyDataYearMouthDay\$MyDataHourMinute.png
+C:\Users\studentwei\scoop\apps\snipaste-beta\current\Snipaste.exe snip --full -o D:\TEMP\PicLog\$MyDataYearMouthDay\$MyDataHourMinute.png
--- ffmpeg_compress_image.ps1
+++ ffmpeg_compress_image.ps1
@@ -1,4 +1,6 @@
 foreach ($element in $args) {
-    $element_bak = $element.Replace(".jpg","_bak.jpg")
-    ffmpeg -i $element -q 2 $element_bak
+    #$element_bak = $element.Replace(".jpg","_bak.jpg")
+    #$element_bak = $element.Replace(".png","_bak.png")	
+    $element_bak = $element.Replace(".mp4","_bak.mp4")
+    ffmpeg -i $element -q 5 $element_bak
 }
